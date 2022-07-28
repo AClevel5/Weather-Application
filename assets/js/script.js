670b6304093efbacecf097538f5f5458819f5e31
Alex Cleveland <Alexcleveland5@gmail.com>
2022-07-27 18:06:12 -0600

    JS Sudo Code

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+//Variables:
+
+
+//Functions:
+//Call to an API to generate Latitude and Longitude based on search box input
+//Pass Lat & Long into Open Weather API to grab response. Only want Current and Daily info back so need logic to cut out hourly, alerts, & minutely
+//Current update: Grab City (date), Temp, Wind, Humidity, UV index and update into state div
+//Daily update: Grab Date, icon, temp, wind, humidty and create a card per day to store the data
+//Store last city searched into local storage
+//create list of cities from local storage
+
+
+//Call Functions:
+
+
+//Event Listeners:
+// Grab input of input box once search is clicked
