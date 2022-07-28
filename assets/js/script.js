316e88cc2115449964778a20c830c12e1f8d4451
Alex Cleveland <Alexcleveland5@gmail.com>
2022-07-27 19:52:03 -0600

    API Fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,22 @@ var inputCity = document.querySelector('#inputCity')
 //https://api.openweathermap.org/data/2.5/weather?q=denver&appid=30be9d6f81ee076d9023bf7273f7e725
 
 //Functions:
-//Grab the City out of the entry field
+//Grab the City out of the entry field and make the API call to grab weather data
 function searchFormSubmit(){
     var inputCityText = inputCity.value.trim();
     console.log(inputCityText)
-    return;
+
+fetch("https://api.openweathermap.org/data/2.5/weather?q=" + inputCityText + "&appid=30be9d6f81ee076d9023bf7273f7e725", {
+  // The browser fetches the resource from the remote server without first looking in the cache.
+  // The browser will then update the cache with the downloaded resource.
+  cache: 'reload',
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
 }
 //Pass City into Open Weather API to grab response. Only want Current and Daily info back so need logic to cut out hourly, alerts, & minutely
 //Current update: Grab City (date), Temp, Wind, Humidity, UV index and update into state div
